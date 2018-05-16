308732959d9e35f0076917d216ef3128a3c4c3c2
Alexander Fellman <alexanderfellman@The-Wonderful-Wombat-of-Oz.local>
2018-05-16 12:22:09 -0400

    fun with lists

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# print("hello world")
+# print("this is me")
+# print("life should be")
+# print("fun for everyone")
+
+hello_str="Saddle Club Theme"
+
+hello_int=21
+
+hello_tuple=(21,32)
+
+hello_list=["Hello World","this is me","life should be","fun for everyone"]
+
+hello_dict={"first_name":"Carole",
+"last_name":"Hanson",
+"horse":"Starlight",
+"favorite_colour":"Purple"}
+
+print(hello_dict)
+print(hello_list)
